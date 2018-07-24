d21cd0c4f1099ed76d6a1a26b991dbb3786b1d26
Davezy <Davezy@DESKTOP-BORM6I1>
2018-07-24 11:38:39 +0700

    GGwp

---
+++ myweb1/src/myweb1/test.java
@@ -0,0 +1,23 @@
+package myweb1;
+
+public class test {
+	
+	private String name;
+	private String lastname;
+	
+	public test() {
+		
+	}
+	public test(String name,String lastname) {
+		this.name=name;
+		this.lastname=lastname;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getlastname() {
+		return lastname;
+	}
+	
+	
+}
